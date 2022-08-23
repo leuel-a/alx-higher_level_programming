c2aac9ce6ad97248af0a1c6e86923b68c93ad20a
nefelibatala1993 <leuela1993@gmail.com>
2022-08-23 23:42:24 +0000

    Added 11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -2,7 +2,7 @@
 def pow(a, b):
     if b < 0:
         b = -1 * b
-        for i in range(1, b):
+        for i in range(1, (b+1)):
             a = a / a
         return a
     for i in range(1, b):
