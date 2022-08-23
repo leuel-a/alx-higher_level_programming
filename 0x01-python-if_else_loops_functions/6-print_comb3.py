3acb28249ccad77555e8f52cf73f2a9649cb7926
nefelibatala1993 <leuela1993@gmail.com>
2022-08-23 22:13:24 +0000

    Fixed error on 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for x in range(0, 10):
     for y in range(0, 10):
         if (x < y):
-            print("{}{}, ".format(x, y), end=' ')
+            print("{}{}, ".format(x, y), end='')
 print("{}{}".format(8, 9))
