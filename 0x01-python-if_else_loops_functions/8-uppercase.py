e65359aa838dd4a7697b7d3d188e6698982f6ed7
nefelibatala1993 <leuela1993@gmail.com>
2022-08-23 22:48:24 +0000

    Added 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+new = ''
+def uppercase(str):
+    for i in str:
+        if (ord(i) > 96 and ord(i) < 123):
+            new = new + chr((ord(i) - 32))
+        else:
+            new = new + i
