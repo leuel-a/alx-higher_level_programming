ec2db7e49fd7225d5e30c4d4915df41770a141a8
nefelibatala1993 <leuela1993@gmail.com>
2022-08-28 07:56:34 +0000

    Attempted to fix error on 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if my_list is None:
-        return
     new = my_list
+    if new is None:
+        return
     if idx < 0 or idx >= len(new):
         return new
     new[idx] = element
