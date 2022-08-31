45ed47d165b58fa086e4de07289f1fad6126d752
nefelibatala1993 <leuela1993@gmail.com>
2022-08-31 20:39:15 +0000

    Passed pycodestyle for 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,5 +3,4 @@ def multiply_by_2(a_dictionary):
     a_dictionary_2 = a_dictionary.copy()
     for x in a_dictionary_2.keys():
         a_dictionary_2[x] *= 2
-    return(a_dictionary_2)
-
+    return (a_dictionary_2)
