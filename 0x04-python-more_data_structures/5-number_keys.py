69799e98a398d5ee76a26a9b4e375c6147d133bb
nefelibatala1993 <leuela1993@gmail.com>
2022-08-31 18:32:48 +0000

    Attempted fix for 5-number_keys.py

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,3 @@
 #/usr/bin/python3
 def number_keys(a_dictionary):
-    number_of_keys = 0
-    for i in a_dictionary:
-        number_of_keys += 1
-    return number_of_keys
+    return(len(a_dictionary.keys()))
