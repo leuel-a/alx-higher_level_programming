7633a7dc808057e94c93d2705ddda30ca62153cd
nefelibatala1993 <leuela1993@gmail.com>
2022-08-31 20:36:23 +0000

    Added 8-multiply_by_2.py

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    a_dictionary_2 = a_dictionary.copy()
+    for x in a_dictionary_2.keys():
+        a_dictionary_2[x] *= 2
+    return(a_dictionary_2)
+
