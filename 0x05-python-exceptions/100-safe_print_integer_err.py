be2521df40c399d19e414a9e14260ffbcee2b697
nefelibatala1993 <leuela1993@gmail.com>
2022-09-13 01:17:56 +0300

    Added 100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,11 +3,10 @@ import sys
 
 
 def safe_print_integer_err(value):
-    bool_val = True
     try:
         print("{:d}".format(value))
     except ValueError as err:
         print("Exception: {}".format(err), file=sys.stderr)
-        bool_val = False
-    finally:
-        return bool_val
+    else:
+        return True
+    return False
