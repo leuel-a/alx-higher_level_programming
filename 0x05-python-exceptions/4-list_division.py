af541707169370ffe93b1c46994d49a8b7d47299
nefelibatala1993 <leuela1993@gmail.com>
2022-09-13 00:11:38 +0300

    Attempted to pass checker for 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -19,5 +19,5 @@ def list_division(my_list_1, my_list_2, list_length):
             aux = 0
         finally:
             new_list.append(aux)
-            i += 1
+        i += 1
     return new_list
