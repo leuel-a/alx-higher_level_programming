705be2cb985f86095b9a844accf99bc7835b4fe9
nefelibatala1993 <leuela1993@gmail.com>
2022-09-13 00:18:09 +0300

    Attempted to pass checker for 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,9 +2,6 @@
 def list_division(my_list_1, my_list_2, list_length):
     i, aux = 0, 0
     new_list = []
-    if list_length <= 0:
-        print("out of range")
-        return new_list
     while i < list_length:
         try:
             aux = my_list_1[i] / my_list_2[i]
