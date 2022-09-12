4f9db083c1b8d857181155e1034565a827b42481
nefelibatala1993 <leuela1993@gmail.com>
2022-09-13 00:16:10 +0300

    Attempted to pass checker for 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,15 +9,13 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             aux = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
-            print("division by zero")
-            aux = 0
+            print("division by 0")
         except TypeError:
             print("wrong type")
-            aux = 0
         except IndexError:
             print("out of range")
-            aux = 0
         finally:
             new_list.append(aux)
-        i += 1
+            aux = 0
+            i += 1
     return new_list
