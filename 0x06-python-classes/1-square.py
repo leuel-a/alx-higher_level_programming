e7804f5aea62519d5aaf1f42804c24d5930df229
nefelibatala1993 <leuela1993@gmail.com>
2022-09-13 18:59:18 +0300

    Added 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+1-square Module
+"""
+
+
+class Square:
+    """
+        Attributes:
+            __size: Size of the square
+    """
+    def __init__(self, size):
+        """
+            Args:
+                size: Size of the square to be instantiated
+        """
+        self.__size = size
+
