524682c6597716260d76baca4de9d41f15cd6069
nefelibatala1993 <leuela1993@gmail.com>
2022-09-19 19:55:53 +0300

    Added 0-rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    This is the "0-rectangle" module.
+
+    The 0-rectangle module supplies a class that defines a rectangle.
+"""
+class Rectangle:
+    """
+        This is the Rectangle class.
+
+        The rectangle class defines a rectangle
+    """
+    pass
