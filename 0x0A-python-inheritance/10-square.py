e8ef3af790f82333127b83d1e0476cc8b1ea6377
nefelibatala1993 <leuela1993@gmail.com>
2022-09-27 03:09:29 +0300

    Added 10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bi/python3
+"""This module has one class, Square class.
+
+The Square class inherit from the 9-rectangle module.
+"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """This is the Square class"""
+
+    def __init__(self, size):
+        """Instatiation Method / Default Constructor
+
+        Args:
+            size (int): size of the square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
