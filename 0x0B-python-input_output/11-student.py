81e8b7d178ec7bad8c84a2ff642dad08c7c7ef6e
nefelibatala1993 <leuela1993@gmail.com>
2022-09-29 16:55:46 +0300

    Added 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,41 +1,57 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""This is the '11-student' module"""
 
 
 class Student:
-    """Represent a student."""
+    """This is the Student class
+
+    Attributes:
+        first_name: the first name of the student
+        last_name: the lat name of the student
+        age: the age of the student
+
+    """
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
+        """Instantiation Method / Default Constructor
 
         Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
+            first_name (str): first name of the new student
+            last_name (str): last name of the new student
+            age: age of the new student
+
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
-
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
-
+        """Retrieves a dictionary representation of the student
+        instance.
         Args:
-            attrs (list): (Optional) The attributes to represent.
+            attrs (list): this limits the data that will be retrieved
+                in the dictionary representation
+        Description:
+            If attrs is a list of strings, only attribute names contained
+            in this list must be retrieved.
         """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+
+        if attrs is None:
+            return self.__dict__
+        Dict = {}
+        for i in self.__dict__:
+            for j in attrs:
+                if i == j:
+                    Dict[j] = self.__dict__[i]
+
+        return Dict
 
     def reload_from_json(self, json):
-        """Replace all attributes of the Student.
+        """Replaces all attributes of a student instance
 
         Args:
-            json (dict): The key/value pairs to replace attributes with.
+            json (dict): dictionary representation of the new
+                attributes that the student instance will assume
+
         """
-        for k, v in json.items():
-            setattr(self, k, v)
+        self.__dict__ = json
