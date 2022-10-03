a66891ee69a1239ff44413498e8ca6563940648a
nefelibatala1993 <leuela1993@gmail.com>
2022-10-03 20:58:31 +0300

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -57,3 +57,15 @@ class Base:
                 for i in list_objs:
                     list_dictionaries.append(i.to_dictionary())
                 f.write(Base.to_json_string(list_dictionaries))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string representation json_string
+
+        Args:
+            json_string (str): string representing a list of dictionaries
+
+        """
+        if json_string is None:
+            return []
+        return json.loads(json_string)
