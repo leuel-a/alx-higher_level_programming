66375fff7bc0b10f6004776160c3fc8be8d97fa1
nefelibatala1993 <leuela1993@gmail.com>
2022-10-03 10:25:51 +0300

    Updated rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -33,3 +33,78 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
+
+    @property
+    def width(self):
+        """Getter for the width of the instance
+
+        Returns:
+            int: the width of the instance
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """Setter for the width of the instance
+
+        Args:
+            width (int): width of the instance
+        """
+        self.__width = width
+
+    @property
+    def height(self):
+        """Getter for the height of the instance
+
+        Returns:
+            int: the height of the instance
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """Setter for the height of the instance
+
+        Args:
+            height (int): height of the instance
+
+        """
+        self.__height = height
+
+    @property
+    def x(self):
+        """Getter for the x attribute of the instance
+
+        Returns:
+            int: the x attribute of the instance
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        """Setter for the x attribute of the instance
+
+        Args:
+            int: the x attribute of the instance
+
+        """
+        self.__x = x
+
+    @property
+    def y(self):
+        """Getter for the y attribute of instance
+
+        Returns:
+            int: the y attribute of the instance
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        """Setter for the y attribute of instance
+
+        Args:
+            int: the y attribute of instance
+
+        """
+        self.__y = y
