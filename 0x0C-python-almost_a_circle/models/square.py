7fb48abdf4d973e5102ec115a463627fb360ec91
nefelibatala1993 <leuela1993@gmail.com>
2022-10-27 11:42:01 +0300

    Updated square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -19,9 +19,24 @@ class Square(Rectangle):
             y (int, optional): y position of new Square instance
             id (int): id of new Square instance
         """
+        self.__size = size
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """Returns a string representation of Square instance"""
         return "[Square] <{}> {}/{} - {}".format(self.id, self.x,
                                                  self.y, self.width)
+
+    @property
+    def size(self):
+        """Getter for size of Square instance"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Setter for size of Square instance"""
+        if not isinstance(size, int):
+            raise TypeError("width must be an integer")
+        if size <= 0:
+            raise ValueError("width must be > 0")
+        self.__size = size
