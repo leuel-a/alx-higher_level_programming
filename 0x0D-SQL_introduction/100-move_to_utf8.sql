cce7633988e2ee52b05076d0cb38b09d357e9a33
nefelibatala1993 <leuela1993@gmail.com>
2022-11-02 00:27:14 +0300

    Fixed 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,9 +1,9 @@
--- Change database to utf-8
--- Use hbtn_0c_0 database
-USE hbtn_0c_0;
--- Change name to utf-8
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
--- Change table to utf-8
+-- converts hbtn_0c_0 database to utf8
+-- table: first_table to utf8
+-- field: name to utf8
+
+ALTER DATABASE htbn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- Change database to utf-8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
