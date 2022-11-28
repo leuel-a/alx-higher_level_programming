b67d826aba6a3c11aff53499d0ea63bf7216eaed
nefelibatala1993 <leuela1993@gmail.com>
2022-11-28 12:40:14 +0300

    Added files

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/**
+ * Prints two arguments passed to it, in the following format: “ is ”
+ *  ->> Example: $ ./4-concat.js c cool
+ *               $ c is cool
+ */
+
+args = process.argv;
+console.log(args[2] + " is " + args[3]);
