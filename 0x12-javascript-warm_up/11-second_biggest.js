d127454a7850a1675f557d4ab3721853c7fe0e29
nefelibatala1993 <leuela1993@gmail.com>
2022-11-30 07:50:27 +0300

    Added 100-let_me_const.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,19 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
+/**
+ * Searches the second biggest integer in the list of arguments.
+ */
+
+const args = process.argv;
+
+if (args.length <= 3) {
   console.log(0);
 } else {
-  const args = process.argv.map(Number)
-    .slice(2, process.argv.length)
-    .sort((a, b) => a - b);
-  console.log(args[args.length - 2]);
+  let max = args[2] * 1; let secondMax = args[3] * 1;
+  for (let i = 2; i < args.length; i++) {
+    if ((args[i] * 1) > max) {
+      secondMax = max;
+      max = args[i] * 1;
+    }
+  }
+  console.log(secondMax);
 }
