acb7123276c833c499771a9610edfc1e959a714a
nefelibatala1993 <leuela1993@gmail.com>
2022-12-15 22:26:24 +0300

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Takes a URL, sends a request to that URL, and displays the size of the body of the response
-
-if [ $# -ne 1 ]
-then
-    echo "Usage: 0-body_size.sh [URL]"
-else
-    curl -w '%{size_download}' -o /dev/null -s "$1" ; echo ""
-fi
+curl -w '%{size_download}' -o /dev/null -s "$1" ; echo ""
