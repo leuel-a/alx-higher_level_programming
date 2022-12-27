4711df3e3ac9587160a23c3dac7e642f712d9b27
nefelibatala1993 <leuela1993@gmail.com>
2022-12-27 09:34:39 +0300

    Passed semistandard

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -6,8 +6,8 @@ const list = require('./100-data').list;
 console.log(list);
 
 // Create a new list by using a map
-const newList = list.map(function(x, i) {
-  return x * i;
-})
+const newList = list.map(function (num, index) {
+  return num * index;
+});
 
 console.log(newList);
