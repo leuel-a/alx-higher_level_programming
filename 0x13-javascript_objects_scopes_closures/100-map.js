8aaeee1fa97398ae87dd54c9c9ebfa6615c65e6b
nefelibatala1993 <leuela1993@gmail.com>
2022-12-27 09:33:02 +0300

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -6,9 +6,8 @@ const list = require('./100-data').list;
 console.log(list);
 
 // Create a new list by using a map
-let i = 0;
-const newList = list.map(function (x) {
-  return x * i++;
-});
+const newList = list.map(function(x, i) {
+  return x * i;
+})
 
 console.log(newList);
