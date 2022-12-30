9df6021e51b52c1973b6f3f41154fcdd970a47b2
nefelibatala1993 <leuela1993@gmail.com>
2022-12-31 00:41:40 +0300

    pass pycodestyle

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,6 +6,7 @@ from sqlalchemy import Column, Integer, String
 # Creating the declarative base class
 Base = declarative_base()
 
+
 class State(Base):
     """Class that links to the states table of the hbtn_0e_6_usa DB"""
     __tablename__ = 'states'
