c85507d4e067085ea900b8b184fe18389b8af8e5
nefelibatala1993 <leuela1993@gmail.com>
2022-12-31 00:29:29 +0300

    add support for command line arguments

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Defines the State class, an instance of Base = declarative_base()"""
+import sys
+from urllib.parse import quote
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
-from urllib.parse import quote
 
 # Creating the declarative base class
 Base = declarative_base()
@@ -14,7 +15,10 @@ class State(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
+# Getting the command line arguments
+args = sys.argv[1:]
+
 # Create the engine for the connection
-user, passwd, host, port, db = 'root', 'la@1993#', 'localhost', 3306, 'hbtn_0e_6_usa'
+user, passwd, host, port, db = args[0], args[1], 'localhost', 3306, args[2]
 Engine = create_engine(f"mysql+mysqldb://{user}:{quote(passwd)}@{host}:{port}/{db}")
 Base.metadata.create_all(Engine)
