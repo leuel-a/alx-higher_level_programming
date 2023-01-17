5c033bc3620931886a56dd192fc7b8828fa40891
nefelibatala1993 <leuela1993@gmail.com>
2023-01-17 05:22:25 +0300

    remove timeout

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ import requests
 
 def main():
     """Entry point of my program"""
-    response = requests.get('https://alx-intranet.hbtn.io/status', timeout=1)
+    response = requests.get('https://alx-intranet.hbtn.io/status')
     print('Body response:')
     print(f'\t- type: {type(response.text)}')
     print(f'\t- content: {response.text}')
