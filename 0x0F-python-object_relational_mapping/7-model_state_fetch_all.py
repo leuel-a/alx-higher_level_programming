0ade297adac23390d251d8606af7ecdf8df98683
nefelibatala1993 <leuela1993@gmail.com>
2023-01-18 21:20:05 +0300

    made the return of query to a list

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,11 +11,10 @@ def main():
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]
     ))
-    Base.metadata.create_all(engine)
     session = sessionmaker(bind=engine)
     session_obj = session()
 
-    for instance in session_obj.query(State).order_by(State.id):
+    for instance in session_obj.query(State).order_by(State.id).all():
         print(f'{instance.id}: {instance.name}')
 
     session_obj.close()
