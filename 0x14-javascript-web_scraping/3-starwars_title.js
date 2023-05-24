fd2ca17a797c5d9daf0053940ef0eb50a4e0159a
nefelibatala1993 <leuela1993@gmail.com>
2023-05-24 22:42:10 +0300

    passed semistandard for all

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,7 +4,6 @@ const request = require('request');
 const id = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/';
 
-
 request(url + id, (error, response, body) => {
   if (error) {
     return console.log(error);
