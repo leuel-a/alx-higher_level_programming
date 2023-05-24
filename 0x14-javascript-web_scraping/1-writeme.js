28d7e54891fb73e3af4fe95cebd1d657fc3198f1
nefelibatala1993 <leuela1993@gmail.com>
2023-05-24 13:44:52 +0300

    finish second task

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filename = process.argv[2];
+const data = process.argv[3];
+
+fs.writeFile(filename, data, 'utf-8', (error) => {
+  if (error) {
+    return console.log(error);
+  }
+});
