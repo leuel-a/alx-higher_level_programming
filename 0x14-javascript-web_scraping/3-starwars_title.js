cd40cb61bcb5d882428325dec85c88104e9c0506
nefelibatala1993 <leuela1993@gmail.com>
2023-05-24 14:10:09 +0300

    Added fourth task

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+
+request(url + id, (error, response, body) => {
+  if (error) {
+    return console.log(error);
+  }
+  console.log(JSON.parse(body).title);
+});
