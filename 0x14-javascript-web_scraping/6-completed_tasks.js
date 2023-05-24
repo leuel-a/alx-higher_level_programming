2c6205ca142ed071cac83d24f2e75e268c144230
nefelibatala1993 <leuela1993@gmail.com>
2023-05-24 22:33:55 +0300

    added seventh task

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -8,7 +8,13 @@ request(url, (error, response, body) => {
     return console.log(error);
   }
 
-  body = JSON.parse(body);
+  // Check if the response does not contain a valid JSON
+  try {
+    body = JSON.parse(body);
+  } catch (error) {
+    return console.log(error);
+  }
+
   const result = {};
   for (const task of body) {
     if (task.userId in result === false) {
