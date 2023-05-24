0ed1e1d357bfbb257a46aeeb59ee501cb7b1ef01
nefelibatala1993 <leuela1993@gmail.com>
2023-05-24 13:35:44 +0300

    update first task

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const file_name = process.argv[2];
 
-fs.readFile(file_name, function (err, data) {
+fs.readFile(file_name, "utf-8", function (err, data) {
   if (err) {
     return console.log(err);
   }
